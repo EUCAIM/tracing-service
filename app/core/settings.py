4f8a1c452d30141837ef2f1ed88c3e2dc5674e17
asalic <asalic@users.noreply.github.com>
2026-09-09 08:38:00 +0200

    settings initted in a function

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,17 +1,21 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 from pydantic import BaseModel
 import tomllib
-import yaml
 from pathlib import Path
 import os
 
 from .settings_common import ENV_VARS_PREFIX, load_settings
 
+_settings = None
 
 def get_settings() -> Settings:
-    if settings is None:
-        raise Exception("Settings with secret not initialized.")
-    return settings
+    global _settings
+    if _settings is None:
+        path = os.getenv(f"{ENV_VARS_PREFIX}APP_SETTINGS", Path(__file__).parent.parent / "resources" / "settings.yaml") 
+        raw_settings = load_settings(path)
+        name, version = load_name_version_from_toml()
+        _settings = Settings(**{"app_name": name, "version": version, "app": raw_settings})
+    return _settings
 
 def load_name_version_from_toml():
     pyproject = Path(__file__).resolve().parents[2] / "pyproject.toml"
@@ -53,21 +57,3 @@ class Settings(BaseSettings):
     app: AppSettings
     app_name: str
     version: str
-    # model_config = SettingsConfigDict(env_prefix=ENV_VARS_PREFIX, env_nested_delimiter="__", env_file=".env", extra="ignore",case_sensitive=False)
-
-# def init_settings(arg_path: str | None) -> None:
-#     path = 
-#     if not os.getenv("APP_SETTINGS") is None: 
-        
-#     elif not arg_path is None:
-#         path = arg_path
-path = os.getenv(f"{ENV_VARS_PREFIX}APP_SETTINGS", Path(__file__).parent.parent / "resources" / "settings.yaml") 
-raw_settings = load_settings(path)
-name, version = load_name_version_from_toml()
-
-settings = Settings(**{"app_name": name, "version": version, "app": raw_settings})
-
-# settings.app_name = name
-# settings.version = version
-# app_settings = AppSettings(**raw_settings)
-# settings.app = app_settings
