3534c06524e1bb0ce84a25dba41795652b7d0cda
asalic <asalic@users.noreply.github.com>
2026-09-18 15:19:09 +0200

    resources don't accept extra fields than defined

--- app/schemas/v2/traces/common.py
+++ app/schemas/v2/traces/common.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from ...common import NonNullOrEmptyStr
 from app.models.v2.common_types import HashType
 
 class CreateDatasetResource(BaseModel):
-
+    model_config = ConfigDict(extra="forbid")
     # The id of the submitted resource with the request to add a trace.
 	# It has to uniquely identify this resource on the system and t has to be anonymized
 	# The uniqueness is not verified by this service.
